5401955a561eaf45b9b07e93d2c0a874a569d368
Minh Ha <minh.ha@flickelectric.co.nz>
2020-07-05 10:19:55 +1200

    use port 5433 for dev postgres12

--- config/dev.exs
+++ config/dev.exs
@@ -6,6 +6,7 @@ config :rumbl, Rumbl.Repo,
   password: "postgres",
   database: "rumbl_dev",
   hostname: "localhost",
+  port:     5433,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
